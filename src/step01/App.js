8b0e7af356bb384380785b7f6c3c54e42fd71e3e
sun <sun25555@gail.com>
2024-07-29 10:53:02 +0900

    test

---
+++ src/step01/App.js
@@ -0,0 +1,42 @@
+// 1.로딩페이지
+import './App.css';
+import {useState, useEffect} from 'react';
+
+
+function App() {
+  //상태변수를 설정
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadCounter, setLoadCounter] = useState(0);
+  useEffect(
+    ()=>{      
+      function IncreaseCounter(){
+        let _counter = loadCounter;
+        _counter = _counter + 1;
+        // 카운터값이 10이상이면 Loading종료, 타이머 종료
+        if(_counter >10){
+          setIsLoading(false);
+          clearInterval(loadTimer);
+        }
+        setLoadCounter(
+          _counter
+        );
+      }
+      //타이머로 loadcounter 값을 1씩 증가
+      const loadTimer = setInterval(
+        IncreaseCounter,1000             
+      );
+      return ()=>{ clearInterval(loadTimer); }
+
+
+    },[loadCounter]
+
+  );
+
+  return (
+    <div >
+      {isLoading? `Loading... ${loadCounter}`: 'Loaded'}
+    </div>
+  );
+}
+
+export default App;
